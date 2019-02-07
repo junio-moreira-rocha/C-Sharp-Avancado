513084101d6b47729bca398f0d14d9d83551a013
Júnio Moreira da Rocha <junio-moreira@hotmail.com>
2019-02-06 23:06:50 -0200

    Delegate como parametro de metodos.

---
+++ CursoCSharpAvancado/DelegateComoParametroDeMetodos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateComoParametroDeMetodos
+{
+    class Program
+    {
+        delegate int ExecutarOperacao(int a, int b);
+
+        private static void EscreverResultado(int a, int b, ExecutarOperacao operacao)
+        {
+            int resultado = operacao(a, b);
+            Console.WriteLine(string.Format("O resultado é {0}", resultado));
+        }
+
+        private static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            EscreverResultado(20, 15, Somar);
+
+            Console.ReadKey(true);
+        }
+    }
+}
